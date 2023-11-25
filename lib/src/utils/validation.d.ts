fd498b8793381b733f36e1eddcea6a5a2e5b2724
Resyntech <carranzagcarlo@gmail.com>
2023-11-26 00:30:17 +0800

    Added failsafe for package script and added role escalation

--- lib/src/utils/validation.d.ts
+++ lib/src/utils/validation.d.ts
@@ -1,3 +1,6 @@
+import { CurrentUserRoleType } from "../types/user";
 /** Strictly validates email with the domain used by BulSU. aka `@bulsu.edu.ph` */
 declare const validateEmail: (email: string) => boolean;
-export { validateEmail };
+/** If it returns faculty, there is a problem */
+declare function recipientEscalation(role: CurrentUserRoleType): CurrentUserRoleType;
+export { validateEmail, recipientEscalation };
--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -1,7 +1,26 @@
+import { CurrentUserRoleType } from "../types/user"
+
 /** Strictly validates email with the domain used by BulSU. aka `@bulsu.edu.ph` */
 const validateEmail = (email: string) => {
   const bulsuRegex = /^[a-z]+(\.[a-z]+)*@bulsu\.edu\.ph$/
   return bulsuRegex.test(email)
 }
+/** If it returns faculty, there is a problem */
+function recipientEscalation(role: CurrentUserRoleType): CurrentUserRoleType {
+  switch (role) {
+    case "student":
+      return "mayor"
+    case "mayor":
+      return "adviser"
+    case "adviser":
+      return "program_chair"
+    case "program_chair":
+      return "department_head"
+    case "department_head":
+      return "board_member"
+    default:
+      return "faculty"
+  }
+}
 
-export { validateEmail }
+export { validateEmail, recipientEscalation }
